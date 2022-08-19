c81c25f53f6e10a70716698f3319bcc972b21a74
Henrique Greghi <henriquevsg@gmail.com>
2022-08-19 09:17:32 -0300

    add classe

--- ImpostoDeRenda/Calculo.cs
+++ ImpostoDeRenda/Calculo.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace ImpostoDeRenda
 {
     class Calculo
     {
+        public double salario;
+      public void CalculoImposto()
+        {
+           
+            double imposto;
+            double fimPrimeiraFaixa = 2000;
+            double fimSegundaFaixa = 3000;
+            double fimTerceiraFaixa = 4500;
 
+            if (salario < fimPrimeiraFaixa)
+            {
+                Console.WriteLine("Isento");
+            }
+            else if (salario < fimSegundaFaixa)
+            {
+                imposto = (salario - fimPrimeiraFaixa) * 0.08;
+                Console.WriteLine("R$" + imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            else if (salario < fimTerceiraFaixa)
+            {
+                imposto = (salario - fimSegundaFaixa) * 0.18 + (1000 * 0.08);
+                Console.WriteLine("R$" + imposto.ToString("F2", CultureInfo.InvariantCulture));
+
+            }
+            else if (salario > fimTerceiraFaixa)
+            {
+                imposto = (salario - fimTerceiraFaixa) * 0.28 + (1500 * 0.18) + (1000 * 0.08);
+                Console.WriteLine("R$" + imposto.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
     }
 }
+
--- ImpostoDeRenda/Program.cs
+++ ImpostoDeRenda/Program.cs
@@ -5,43 +5,18 @@ namespace ImpostoDeRenda
 {
     class Program
     {
-        
+
 
 
         static void Main(string[] args)
         {
-            
+            Calculo calc = new Calculo();
 
-            Console.WriteLine("Entre com o valor de seu salario:" );
-            double salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com o valor de seu salario:");
+            calc.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double imposto;
-            double fimPrimeiraFaixa = 2000;
-            double fimSegundaFaixa = 3000;
-            double fimTerceiraFaixa = 4500;
+            calc.CalculoImposto();
 
-            
-            if(salario < fimPrimeiraFaixa)
-            {
-                Console.WriteLine("Isento");
-            }
-            else if (salario < fimSegundaFaixa )
-            {
-                imposto = (salario - fimPrimeiraFaixa) * 0.08;
-                Console.WriteLine("R$" + imposto.ToString("F2",CultureInfo.InvariantCulture));
-            }
-            else if (salario < fimTerceiraFaixa)
-            {
-                imposto = (salario - fimSegundaFaixa) * 0.18 + (1000 * 0.08);
-                Console.WriteLine("R$" + imposto.ToString("F2", CultureInfo.InvariantCulture));
-
-            }
-            else if (salario > fimTerceiraFaixa)
-            {
-                imposto = (salario - fimTerceiraFaixa) * 0.28 + (1500 * 0.18) + (1000 * 0.08);
-                Console.WriteLine("R$" + imposto.ToString("F2", CultureInfo.InvariantCulture));
-            }
-        
         }
     }
 }
